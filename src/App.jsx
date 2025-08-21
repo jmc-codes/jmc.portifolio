9e5865ba6116cd3e84c6bc23ede2f6ab1adae826
jmc-codes <johnathanmaravilha66@gmail.com>
2025-08-21 18:05:51 +0000

    alteração do script do para new App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import './App.css';
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+// Importando componentes
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Sobre from './components/Sobre';
-import Formacao from './components/Formacao';
 import Experiencia from './components/Experiencia';
+import Formacao from './components/Formacao';
 import Competencias from './components/Competencias';
 import Idiomas from './components/Idiomas';
 import Certificacoes from './components/Certificacoes';
+import Projetos from './components/Projetos';
 import Contato from './components/Contato';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Hero />
-      <Sobre />
-      <Formacao />
-      <Experiencia />
-      <Competencias />
-      <Idiomas />
-      <Certificacoes />
-      <Contato />
+      <Header />
+      <main>
+        <Hero />
+        <Sobre />
+        <Experiencia />
+        <Formacao />
+        <Competencias />
+        <Idiomas />
+        <Certificacoes />
+        <Projetos />
+        <Contato />
+      </main>
       <Footer />
     </>
   );

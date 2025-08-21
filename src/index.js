dbf51e087e78bd8fc1d1f95e128d9bcd107b5ba6
jmc-codes <johnathanmaravilha66@gmail.com>
2025-08-21 09:42:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // se tiver estilos globais
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

4cd7dc0ed396768a80b1792f8c6971ea930632f8
jmc-codes <johnathanmaravilha66@gmail.com>
2025-08-25 16:07:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-// Importando componentes
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Sobre from './components/Sobre';
-import Experiencia from './components/Experiencia';
-import Formacao from './components/Formacao';
-import Competencias from './components/Competencias';
-import Idiomas from './components/Idiomas';
-import Certificacoes from './components/Certificacoes';
-import Projetos from './components/Projetos';
-import Contato from './components/Contato';
-import Footer from './components/Footer';
+import About from './components/About';
+import Experience from './components/Experience';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import AnimatedBackground from './components/AnimatedBackground';
+import ScrollProgressBar from './components/ScrollProgressBar';
+import CursorFollower from './components/CursorFollower';
+import PageTransitions from './components/PageTransitions';
 
 function App() {
   return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <Sobre />
-        <Experiencia />
-        <Formacao />
-        <Competencias />
-        <Idiomas />
-        <Certificacoes />
-        <Projetos />
-        <Contato />
-      </main>
-      <Footer />
-    </>
+    <PageTransitions>
+      <div className="App relative">
+        <AnimatedBackground />
+        <CursorFollower />
+        <ScrollProgressBar />
+        <Header />
+        <main className="relative z-10">
+          <Hero />
+          <About />
+          <Experience />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+      </div>
+    </PageTransitions>
   );
 }
 

cc01af71d739573ae9c0df9c23cec772201e168d
jmc-codes <johnathanmaravilha66@gmail.com>
2025-08-27 15:30:24 +0000

    Atualiza estrutura do projeto e adiciona novos arquivos

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: './jmc-portfolio/',
+  base: '/jmc.portifolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

eba2114ace005d46bb7c342e0543ec7f52661ad5
jmc-codes <johnathanmaravilha66@gmail.com>
2025-08-28 12:45:40 +0000

    Update arqs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // se tiver estilos globais
+import './index.css'; // estilos globais
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
